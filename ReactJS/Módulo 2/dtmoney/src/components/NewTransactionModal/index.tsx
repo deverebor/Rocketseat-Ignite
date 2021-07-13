949a1ddf20a81ae69daa26449f068b606048e961
Lucas Souza <lucasp.sdev@gmail.com>
2021-07-13 12:03:19 -0300

    14 - Estilizando o Modal
    Criado a estilização do modal no NewTrasactionModal/styles.ts ;
    Adesão do react-modal-close(botão) e sua estilização;

--- ReactJS/Módulo 2/dtmoney/src/components/NewTransactionModal/index.tsx
+++ ReactJS/Módulo 2/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,41 +1,44 @@
-import Modal from 'react-modal'
-import { Container } from './styles'
+import Modal from "react-modal";
+
+import closeImg from "../../assets/close.svg";
+
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
-  isOpen: boolean,
-  onRequestClose: () => void,
+  isOpen: boolean;
+  onRequestClose: () => void;
 }
 
-export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
   return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button 
+      type="button" 
+      onClick={onRequestClose} 
+      className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
 
-    <Modal 
-        isOpen={isOpen} 
-        onRequestClose={onRequestClose}
-        overlayClassName="react-modal-overlay"
-        className="react-modal-content"
-        >
-          <Container>
-            <h2>Cadastrar informação</h2>
+      <Container>
+        <h2>Cadastrar informação</h2>
 
-            <input
-              placeholder="Título"
-            />
+        <input placeholder="Título" />
 
-            <input
-              type="number"
-              placeholder="Valor" 
-            />
+        <input type="number" placeholder="Valor" />
 
-            <input
-              placeholder="Categoria" 
-            />
+        <input placeholder="Categoria" />
 
-            <button type="submit">
-              Cadastrar
-            </button>
-          </Container>
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
-
-  )
+  );
 }
